ca6b364e5e878fef761afad4614c969e3a5ab974
Suresh <suresh@ashank.com>
2021-11-01 10:53:02 +0530

    Chnaged AWS and Mob to 32bit and also introduced switch to start the process

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,68 @@ Preferences devPreferences;
 HTTPClient httpClient;
 int ledPin = 2;
 
-String host = "http://192.168.1.8/Esp32Initialize/initializeEspAudio?macId=";
+//String host = "http://192.168.1.8/Esp32Initialize/initializeEspAudio?macId=";
+String host = "http://192.168.2.3:8080/OnidaAuDataV0L/newThingSevlet?mac=";
+//String host = "http://192.168.0.105:8080/OnidaAuDataV0L/newThingSevlet?mac=";
 
 String projStr = "SURESH";
-String ssidStr = "ashwin_hall";
-String passStr = "ashwin691";
+String ssidStr = "GuruVideos"; //"ACT102566495642";//"ashwin_hall";
+String passStr = "9845191688"; //"11215219";//"ashwin691";
 
 String serialNumberStr = "";
 String activationCodeStr = "";
 String awsCodeStr = "";
 String mobCodeStr = "";
 String securityStr = "";
+
+boolean switchDetect(int num)
+{
+  unsigned long debounceStartTime = millis();
+  int keyDebounceCount = 1;
+  int switchPin = 0; //Switch on board
+  pinMode(switchPin, INPUT);
+  int ledPin1 = 2; //Blue Led on board
+  int ledStatus = 1;
+
+  while ((keyDebounceCount > 0) && (millis() < (debounceStartTime + 6000)))
+  {
+    delay(100);
+    if ((digitalRead(switchPin)) == LOW)
+    {
+      keyDebounceCount++;
+      //Serial.println("Switch is LOW");
+    }
+    else
+    {
+      keyDebounceCount--;
+      //Serial.println("Switch is HIGH");
+    }
+    if (ledStatus == 0)
+    {
+      ledStatus = 1;
+      digitalWrite(ledPin1, HIGH);
+    }
+    else
+    {
+      ledStatus = 0;
+      digitalWrite(ledPin1, LOW);
+    }
+    if (keyDebounceCount > num){
+      break;
+    }
+  }
+  //Serial.print("Count = ");
+  //Serial.println(keyDebounceCount);
+  if (keyDebounceCount > num)
+  {
+    Serial.println("Key detected !!! Stopping");
+    return true;
+  }
+  //did not detect Key Sense
+  digitalWrite(ledPin1, HIGH);
+  //Serial.println("Did Not detect key!!!!");
+  return false;
+}
 
 void setup()
 {
@@ -28,8 +79,10 @@ void setup()
 
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
-  char ssid[] = "ashwin_hall";
-  char pass[] = "ashwin691";
+  // char ssid[] = "ashwin_hall";
+  // char pass[] = "ashwin691";
+  char ssid[] = "GuruVideos"; //"ACT102566495642";
+  char pass[] = "9845191688"; //"11215219";
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
@@ -58,6 +111,15 @@ void setup()
     }
   }
   digitalWrite(ledPin, HIGH);
+  Serial.println("Connected to Server");
+
+  Serial.println("Waiting for Switch to be pressed");
+  //Check for Switch press
+  while (!switchDetect(3))
+  {
+    //Wait for Key press
+  }
+  Serial.println("Switch press detected");
 
   //Get Mac ID
   String macID = WiFi.macAddress();
@@ -67,6 +129,8 @@ void setup()
 
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
+  delay(100);
+
   // const int httpPort = 80;
   //http://192.168.1.8/Esp32Initialize/initilizeEspAudio?macId=B4:E6:2D:8D:73:C1
   httpClient.begin(host);
@@ -86,11 +150,16 @@ void setup()
       String payload = httpClient.getString();
       delay(100);
       // Serial.println(payload);
-      serialNumberStr = payload.substring(0, 10);
-      activationCodeStr = payload.substring(10, 20);
-      mobCodeStr = payload.substring(20, 32);
-      awsCodeStr = payload.substring(32, 44);
-      securityStr = payload.substring(44, 364);
+      // serialNumberStr = payload.substring(0, 10);
+      // activationCodeStr = payload.substring(10, 20);
+      // mobCodeStr = payload.substring(20, 32);
+      // awsCodeStr = payload.substring(32, 44);
+      // securityStr = payload.substring(44, 364);
+      serialNumberStr = payload.substring(0, 14);
+      activationCodeStr = payload.substring(14, 24);
+      awsCodeStr = payload.substring(24, 56);
+      mobCodeStr = payload.substring(56, 88);
+      securityStr = payload.substring(88, 408);
     }
   }
   else
@@ -108,12 +177,12 @@ void setup()
   //Clear partion
   /* TESTING if Partion is erased!!! */
 
-  // Serial.println("BEFORE Project from memory = " + devPreferences.getString("project"));
-  // Serial.println("BEFORE Serial Number from memory = " + devPreferences.getString("serialNumber"));
-  // Serial.println("BEFORE Activation Code from memory = " + devPreferences.getString("activationCode"));
-  // Serial.println("BEFORE SSID from memory = " + devPreferences.getString("ssid"));
-  // Serial.println("BEFORE PASSWORD from memory = " + devPreferences.getString("pass"));
-  // Serial.println("BEFORE SHA Digest loaded from Memory = " + devPreferences.getString("securityCode"));
+  Serial.println("BEFORE Project from memory = " + devPreferences.getString("project"));
+  Serial.println("BEFORE Serial Number from memory = " + devPreferences.getString("serialNumber"));
+  Serial.println("BEFORE Activation Code from memory = " + devPreferences.getString("activationCode"));
+  Serial.println("BEFORE SSID from memory = " + devPreferences.getString("ssid"));
+  Serial.println("BEFORE PASSWORD from memory = " + devPreferences.getString("pass"));
+  Serial.println("BEFORE SHA Digest loaded from Memory = " + devPreferences.getString("securityCode"));
 
   devPreferences.putString("project", projStr);
   delay(100);
@@ -125,10 +194,10 @@ void setup()
   delay(100);
   devPreferences.putString("activationCode", activationCodeStr);
   delay(100);
-  // devPreferences.putString("awsCode", awsCodeStr);
-  // delay(100);
-  // devPreferences.putString("mobCode", mobCodeStr);
-  // delay(100);
+  devPreferences.putString("awsCode", awsCodeStr);
+  delay(100);
+  devPreferences.putString("mobCode", mobCodeStr);
+  delay(100);
   devPreferences.putString("securityCode", securityStr);
   delay(100);
   devPreferences.end();
